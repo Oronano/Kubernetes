73f75070b1f7159f8f63a6f4a3215de7f67290f7
andreUbuntu <andrecoelho136@gmail.com>
2024-11-04 12:55:27 +0100

    fourth test hook

--- webhook.sh
+++ webhook.sh
@@ -7,7 +7,7 @@ NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].publi
 WEBHOOK_URL="$NGROK_URL/github-webhook"
 
 # GitHub webhook configuration (replace with your values)
-GITHUB_REPO="Oronano/Kubernetes.git"
+GITHUB_REPO="Oronano/Kubernetes"
 GITHUB_TOKEN="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"
 
 # Configure the webhook via GitHub API
